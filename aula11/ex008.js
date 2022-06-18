5aa0b6f7c4c1ecc42a2c49892d4b39ceae874d83
Matheus Duarte <107056365+maxthexus@users.noreply.github.com>
2022-06-18 19:17:41 -0300

    update

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 10.5
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60){ //condição simples
+    console.log(`Você ultrapassou a velocidade Permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
