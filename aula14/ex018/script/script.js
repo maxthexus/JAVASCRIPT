ec4520868536cf008859672b4743fb53cf9a8982
Matheus Duarte <107056365+maxthexus@users.noreply.github.com>
2022-06-20 12:46:35 -0300

    update

---
+++ aula14/ex018/script/script.js
@@ -0,0 +1,19 @@
+function contar(){
+    var inicio = document.getElementById('numinicio')
+    var fim = document.getElementById('numfim')
+    var passo = document.getElementById('numpasso')
+    var res = document.getElementById('res')
+
+    var n1 = Number(inicio.value)
+    var n2 = Number(fim.value)
+    var n3 = Number(passo.value)
+
+    window.alert
+
+    for (var i = 0; i < 9; i++){
+
+    window.alert(`${c}`)
+
+}
+
+}
