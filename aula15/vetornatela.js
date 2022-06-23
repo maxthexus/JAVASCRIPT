1602e9bf2090da12db98a668b31b46dbb49a52ee
Matheus Duarte <107056365+maxthexus@users.noreply.github.com>
2022-06-23 14:38:36 -0300

    up

---
+++ aula15/vetornatela.js
@@ -0,0 +1,11 @@
+let num = [5,8,4,9,3]
+/*
+console.log(num)
+
+for(let i=0; i<num.length; i++){
+    console.log(`esse é o numero na posição ${i} do vetor = ${num[i]}`)
+    }
+*/
+for(let i in num){
+    console.log(`esse é o numero na posição ${i} do vetor = ${num[i]}`)
+}    
