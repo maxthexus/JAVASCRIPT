b9a2ea10738b2c67aac9e99b5f103ed7a30f18e9
Matheus Duarte <107056365+maxthexus@users.noreply.github.com>
2022-06-25 12:04:11 -0300

    update

--- aula17/ex027/script/script.js
+++ aula17/ex027/script/script.js
@@ -1,16 +1,30 @@
-function adicionar(){
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-    let num = document.getElementById('num')
-    if(num.value.length == 0){        
-        window.alert('digite um número!')
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <=100){
+        return true
     }else{
-        let n = []       
-        n.push(num)
-    }window.alert('ola )
+        return false
     }
+}
 
-function finalizar(){
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
 
-    
+function adicionar{
+    if(isNumero(num.value) && !inLista(num.value)){
+        window.alert('Tudo Ok')
 
-}    
+    }else{
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
